43cdecb98eed7202a274130be854978925971e3f
Iarlen Aquiles <iarlensilva775@gmail.com>
2022-10-22 15:28:18 -0300

    added migrations

---
+++ src/migrations/1666395025602-CourseRefactoring.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CourseRefactoring1666395025602 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
